d9db539186267cb85e6424bfb8e8dac0541d3d45
Anas Iqbal <iqbalanas978@gmail.com>
2026-02-07 01:29:20 +0500

    deployment 2

--- backend/main.py
+++ backend/main.py
@@ -24,7 +24,7 @@ app = FastAPI(
 # CORS middleware for frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    allow_origins=["http://localhost:5173", "http://localhost:3000","https://banking-app-plum.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
